df0efc80308f0f48b2a27437dca5af5e46a47edd
Magginho <vicfemagdesian@gmail.com>
2025-04-20 14:54:12 -0300

    chore: add TailwindCSS and PostCSS configs manually

---
+++ client/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{vue,js,ts,jsx,tsx}'
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Inter', 'sans-serif']
+        },
+        colors: {
+          primary: {
+            DEFAULT: '#7c3aed',
+            light:   '#a78bfa',
+            dark:    '#5b21b6'
+          }
+        }
+      }
+    },
+    plugins: []
+  }
+  
