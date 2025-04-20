c87f9800c6ea537d07e1d478f9f7a7517d6d3784
Magginho <vicfemagdesian@gmail.com>
2025-04-20 14:28:06 -0300

    feat: add ModeSelect component

---
+++ client/src/components/ModeSelect.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="mode-select p-4 flex justify-center gap-4">
+      <button
+        class="px-4 py-2 bg-green-500 rounded hover:bg-green-600"
+        @click="join(2)"
+      >
+        1×1
+      </button>
+      <button
+        class="px-4 py-2 bg-blue-500 rounded hover:bg-blue-600"
+        @click="join(3)"
+      >
+        1×1×1
+      </button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { useLobbyStore } from '../stores/lobby'
+  const lobby = useLobbyStore()
+  
+  function join(mode) {
+    lobby.join(mode)
+  }
+  </script>
+  
