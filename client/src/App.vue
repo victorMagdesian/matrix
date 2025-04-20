52e72182a1852b87016d2b7868b04851c101f749
Magginho <vicfemagdesian@gmail.com>
2025-04-20 14:34:15 -0300

    feat: conditional rendering between ModeSelect, WaitingRoom and GameRoom in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app" class="min-h-screen flex items-center justify-center">
+    <ModeSelect    v-if="status === 'idle'"    />
+    <WaitingRoom   v-else-if="status === 'waiting'" />
+    <GameRoom      v-else-if="status === 'matched'" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup>
+import { computed } from 'vue'
+import { useLobbyStore } from './stores/lobby'
+import ModeSelect    from './components/ModeSelect.vue'
+import WaitingRoom   from './components/WaitingRoom.vue'
+import GameRoom      from './components/GameRoom.vue'
+
+const lobby = useLobbyStore()
+const status = computed(() => lobby.status)
+</script>
+
+<style>
+/* só pra centralizar melhor, depois você personaliza */
+#app { background: #1a1a1a; color: #fff; }
 </style>
